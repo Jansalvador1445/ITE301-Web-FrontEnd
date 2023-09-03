66c7f6c2bc35c4ba989b9c49e17e95c84531d93b
Jan Salvador Sebastian <sebastian@mclinica.com>
2023-09-03 11:29:23 +0800

    Added linter

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form>
-        <input type="text" placeholder="Username" value="" class="" id="" >
-        <input type="password" placeholder="password"  value="" class="" id="">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <form>
+            <input type="text" placeholder="Username" value="" class="" id="" >
+            <input type="password" placeholder="password"  value="" class="" id="">
 
-        <button type="submit" class="" id="">Login</button>
-    </form>
-</body>
+            <button type="submit" class="" id="">Login</button>
+        </form>
+    </body>
 </html>
